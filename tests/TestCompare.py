8979867df09206a963377be83c206ec400a3a9d7
kprocopio <kevtprocopio@gmail.com>
2016-07-05 12:04:14 -0600

    Added in Schur_Complement first try.
    This includes a test (in the "tests" folder) which is written in python.
    The "SweeperSchurBoundary.cc" file contains the current version of my attempt at
    the Schur algorithm. It is effectively the same as PBJ, except that it ignores a majority
    of the psi values on the interiors of the domains and instead only iterates based on the boundary
    values. It runs and returns the same values as Sweeper2 and SweeperPBJ (based on the test I wrote),
    but it takes more iterations than PBJ for some reason. Currently, the output it is saving for the tests
    is text files (to make it human-readable while I was troubleshooting). I need to make them binary files to
    save space and add a toggle (probably in input.deck) to turn writing results on/off.

---
+++ tests/TestCompare.py
@@ -0,0 +1,57 @@
+#This compares two files line by line to determine if they are within a given tolerance
+
+tol = float(0.00001)
+
+
+#prompts for filenames
+file1 = raw_input("Enter file 1: ")
+file2 = raw_input("Enter file 2: ")
+out = raw_input("Enter file to write results to: ")
+
+#open files
+f1=open(file1, 'r')
+f2=open(file2, 'r')
+outfile = open(out, 'w')
+
+#read first line of files
+f1line = f1.readline()
+f2line = f2.readline()
+
+#initialize counters
+counter = 1
+numzeros = 0
+numones = 0
+
+#check end of file
+while f1line != '' or f2line != '':
+	#remove whitespace
+	f1line=f1line.rstrip()
+	f2line=f2line.rstrip()
+	#compare	
+	if abs(float(f1line)-float(f2line))<=tol:
+		outfile.write('0\n')
+		numzeros += 1
+	elif abs(float(f1line)-float(f2line))>tol:
+		outfile.write('1\n')
+		numones += 1
+	else:
+		outfile.write('ERROR!')
+
+	#read the next line
+	f1line=f1.readline()
+	f2line=f2.readline()
+
+	#increment
+	counter += 1
+
+outfile.close()
+f1.close()
+f2.close()
+
+percent = numzeros/(numzeros+numones)
+
+print("Number of Correct Results: " + str(numzeros) + '\n')
+print("Number of Incorrect Results: " + str(numones) + '\n')
+	
+		
+
