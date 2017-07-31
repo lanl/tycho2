a9f7f5291e2e8cf99ab98ff58d7de22dfa42df18
Kris Garrett <ckgarrett@lanl.gov>
2017-07-31 16:55:31 -0600

    New CCS setup.

---
+++ run.sh
@@ -0,0 +1,12 @@
+# This is an example run script to automate creating a parallel mesh and
+# running the sweeper on it.
+
+
+NUM_PARTS=20
+IN_FILE="./util/cube-67249.smesh"
+OUT_FILE="temp.pmesh"
+INPUT_DECK="input.deck"
+export OMP_NUM_THREADS=1
+
+./util/PartitionMetis.x $NUM_PARTS $IN_FILE $OUT_FILE
+mpirun -n $NUM_PARTS ./sweep.x $OUT_FILE $INPUT_DECK
